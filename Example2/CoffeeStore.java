e480c0166b28d78b9905579d0b20701bbe99b848
charsyam <charsyam@gmail.com>
2018-05-27 01:22:56 +0900

    Example2

---
+++ Example2/CoffeeStore.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CoffeeStore {
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+        Barista brista = new Barista();
+
+        List<MenuItem> coffeeMenu = new ArrayList<MenuItem>();
+        coffeeMenu.add(new MenuItem("americano", 3000));
+        coffeeMenu.add(new MenuItem("caffelatte", 4000));
+        coffeeMenu.add(new MenuItem("cafuchino", 4000));
+
+        Menu menu = new MapTypeMenu(coffeeMenu);
+
+        customer.order("americano", menu, brista);
+    }
+}
---
+++ Example2/Menu.java
@@ -0,0 +1,5 @@
+import java.util.Map;
+
+public interface Menu {
+    MenuItem choose(String name);
+}
